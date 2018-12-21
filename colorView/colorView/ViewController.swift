d315f0b665d97586d4ba18765ec78ff572c69f0f
Xelaflash <xelaflash@gmail.com>
2018-12-21 14:30:15 -0400

    course projects

--- colorView/colorView/ViewController.swift
+++ colorView/colorView/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var quoteLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        quoteLabel.textColor = .green
+        colorView.backgroundColor = .black
     }
-
-
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            quoteLabel.textColor = .yellow
+            colorView.backgroundColor = .blue
+        } else {
+            quoteLabel.textColor = .purple
+            colorView.backgroundColor = .red
+        }
+    }
+    
 }
+
+
 
--- quoteGenerator/Teki/ViewController.swift
+++ quoteGenerator/Teki/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
     let celeb = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le Karl Lagarfeld", "la Scarlett Johansson", "le Claudy Faucan", "le Sim", "le Noel Flantier", "la Natacha", "la mère de Charlie", "la chatte à Dédé", "mon ptit dim"]
     let activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party", "de mon cul", "de la Belgique", "de rien", "de l'apéro", "du porno Ouzbek", "de la teuf", "de la branlette"]
     
+    
     @IBOutlet weak var quoteLabel: UILabel!
+    
     
     @IBAction func changeQuote() {
         // On séléctionne un élément alétoire parmi les célébrités
-        let randomIndex1 = Int(arc4random_uniform(UInt32(celeb.count)))
+    let randomIndex1 = Int(arc4random_uniform(UInt32(celeb.count)))
         let celebrity = celeb[randomIndex1]
 
         // selection activités
