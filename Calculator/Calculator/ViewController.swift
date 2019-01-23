cfc2c737ae1dd380c5e105159ec0cbb1276c0cfa
Xelaflash <xelaflash@gmail.com>
2019-01-23 11:06:04 -0400

    calculator working but needs improvment (enum + handling digit numbers)

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,45 +9,93 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    var _operationType : Character = " "
+    var _previousNumber : Double = 0
+    var _shouldResetCurrentNumber = true
+    
+//    use of _ for class variables (convention)
+    var _currentNumber : Double = 0 {
+        didSet {
+            ui_currentNumberLabel.text = "\(_currentNumber)"
+        }
+    }
+    
+//    Use of ui_ for all ui variables
     @IBOutlet weak var ui_currentNumberLabel: UILabel!
     
+    func performWaitingCalculous() {
+        let result:Double
+        if _operationType == "+" {
+            result = _previousNumber + _currentNumber
+        } else if _operationType == "-" {
+            result = _previousNumber - _currentNumber
+        } else if _operationType == "-" {
+            result = _previousNumber - _currentNumber
+        } else if _operationType == "*" {
+            result = _previousNumber * _currentNumber
+        } else if _operationType == "/" {
+            result = _previousNumber / _currentNumber
+        } else {
+            result = _currentNumber
+        }
+        _previousNumber = result
+        _shouldResetCurrentNumber = true
+        ui_currentNumberLabel.text = "\(result)"
+    }
+    
     @IBAction func divide() {
+        if _shouldResetCurrentNumber == false {
+            performWaitingCalculous()
+        }
+        _operationType = "/"
     }
     
     @IBAction func multiply() {
+        if _shouldResetCurrentNumber == false {
+            performWaitingCalculous()
+        }
+         _operationType = "*"
     }
     
     @IBAction func sum() {
+        if _shouldResetCurrentNumber == false {
+            performWaitingCalculous()
+        }
+         _operationType = "+"
     }
     
     @IBAction func substract() {
+        if _shouldResetCurrentNumber == false {
+            performWaitingCalculous()
+        }
+         _operationType = "-"
     }
     
     @IBAction func displayResult() {
+        performWaitingCalculous()
     }
     
     @IBAction func resetValue() {
+        _previousNumber = 0
+        _currentNumber = 0
+        _operationType = " "
     }
     
     @IBAction func changeSign() {
+        _currentNumber = _currentNumber * -1
     }
     
     @IBAction func applyPercent() {
+        _currentNumber = _currentNumber / 100
     }
   
     @IBAction func digitButtonTouched(_ sender: UIButton) {
-        ui_currentNumberLabel.text = "\(sender.tag)"
+        if _shouldResetCurrentNumber == true {
+            _currentNumber = Double(sender.tag)
+            _shouldResetCurrentNumber = false
+        } else {
+        _currentNumber = _currentNumber * 10 + Double(sender.tag)
+        }
     }
-    
-
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
 }
-
