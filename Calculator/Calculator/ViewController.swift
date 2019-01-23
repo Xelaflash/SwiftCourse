7449f583ed53323645c4bef5fd21643d06bfe8fa
Xelaflash <xelaflash@gmail.com>
2019-01-23 10:13:49 -0400

    set up ui + outlets +actions

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,7 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var ui_currentNumberLabel: UILabel!
+    
+    @IBAction func divide() {
+    }
+    
+    @IBAction func multiply() {
+    }
+    
+    @IBAction func sum() {
+    }
+    
+    @IBAction func substract() {
+    }
+    
+    @IBAction func displayResult() {
+    }
+    
+    @IBAction func resetValue() {
+    }
+    
+    @IBAction func changeSign() {
+    }
+    
+    @IBAction func applyPercent() {
+    }
+  
+    @IBAction func digitButtonTouched(_ sender: UIButton) {
+        ui_currentNumberLabel.text = "\(sender.tag)"
+    }
+    
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
