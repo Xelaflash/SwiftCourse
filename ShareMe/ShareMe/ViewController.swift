c1512be4a9595d31c37559d484bac7049842ccaf
Xelaflash <xelaflash@gmail.com>
2019-01-25 11:36:49 -0400

    ui review + share btn connexion

--- ShareMe/ShareMe/ViewController.swift
+++ ShareMe/ShareMe/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var textInputField: UITextView!
+    
+    
+    @IBAction func ShareButtonTouched(_ sender: Any) {
+        let shareController = UIActivityViewController(activityItems: ["test"], applicationActivities : nil)
+        present(shareController, animated: true, completion: nil)
     }
 
-
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.textInputField.layer.borderWidth = 0.5
+        self.textInputField.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.textInputField.layer.cornerRadius = 5.0
+    }
 }
 
