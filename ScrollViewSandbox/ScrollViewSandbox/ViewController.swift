7ea70f7d00f4d66e3c7e272c221841564fc633a5
Xelaflash <xelaflash@gmail.com>
2019-01-29 09:38:44 -0400

    sandbox woorking

--- ScrollViewSandbox/ScrollViewSandbox/ViewController.swift
+++ ScrollViewSandbox/ScrollViewSandbox/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
@@ -17,14 +17,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var textFieldB: UITextField!
     @IBOutlet weak var textFieldC: UITextField!
 
+    var activeField: UITextField?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // usefull to dismiss keyboard on return
+        self.textFieldA.delegate = self
+        self.textFieldB.delegate = self
+        self.textFieldC.delegate = self
+        
         // setup keyboard event
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name:UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name:UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name:UIResponder.keyboardWillHideNotification, object: nil)
+        
+//        listen to a tap outside text field to dismiss keyboard
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
     
+    // function to dismiss keyboard on tap
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    // function to dismiss keyboard on hitting return key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
     
     @objc func keyboardWillShow(notification:NSNotification){
         var userInfo = notification.userInfo!
@@ -39,5 +61,7 @@ class ViewController: UIViewController {
         let contentInset:UIEdgeInsets = UIEdgeInsets.zero
         scrollView.contentInset = contentInset
     }
+    
+
     
 }
