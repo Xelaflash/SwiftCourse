b51ced7d95b5e0c6fe22692d3c098f12a1a2effc
Xelaflash <xelaflash@gmail.com>
2019-01-29 11:57:58 -0400

    keyboard inset offset ok

--- TaleOfTheTapeConverter/TaleOfTheTapeConverter/ViewController.swift
+++ TaleOfTheTapeConverter/TaleOfTheTapeConverter/ViewController.swift
@@ -16,7 +16,7 @@ extension Double {
     }
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 //  scrollView and contentView
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
@@ -39,6 +39,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ui_outputInchLabel: UILabel!
     @IBOutlet weak var ui_outputCmLabel: UILabel!
+    
+    var activeField: UITextField?
     
 //    getting the value of the inputs
     func getInputKgValue() -> Double? {
@@ -148,4 +150,47 @@ class ViewController: UIViewController {
         convertCentimetersInputvalue()
     }
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // usefull to dismiss keyboard on return
+        self.ui_inputWeightValueField.delegate = self
+        self.ui_inputHeightValueField.delegate = self
+        self.ui_inputReachValueField.delegate = self
+        
+        // setup keyboard event
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name:UIResponder.keyboardWillHideNotification, object: nil)
+        
+        //        listen to a tap outside text field to dismiss keyboard
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    
+    // function to dismiss keyboard on tap
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
+    // function to dismiss keyboard on hitting return key
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    @objc func keyboardWillShow(notification:NSNotification){
+        var userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+        var contentInset:UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        scrollView.contentInset = contentInset
+    }
+    
+    @objc func keyboardWillHide(notification:NSNotification){
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
 }
+
