be21f18b26777af1a4f59c5daa915e74beebeb1e
Xelaflash <xelaflash@gmail.com>
2019-02-04 11:37:25 -0400

    new game starting done and working

--- Quizzy/Quizzy/Controller/ViewController.swift
+++ Quizzy/Quizzy/Controller/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionView: QuestionView!
+    
+    var game = Game()
+    
+    @IBAction func didTapNewGameButton() {
+        startNewGame()
+    }
+    
+    private func startNewGame() {
+        activityIndicator.isHidden = false
+        newGameButton.isHidden = true
+        questionView.title = "Loading..."
+        questionView.style = .standard
+        scoreLabel.text = " 0 / 10"
+        
+        game.refresh()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let name = Notification.Name(rawValue: "QuestionsLoaded")
+        NotificationCenter.default.addObserver(self, selector: #selector(questionsLoaded), name: name, object: nil)
     }
-
-
+    
+     @objc func questionsLoaded() {
+        activityIndicator.isHidden = true
+        newGameButton.isHidden = false
+        questionView.title = game.currentQuestion.title
+    }
 }
 
--- Quizzy/Quizzy/View/QuestionView.swift
+++ Quizzy/Quizzy/View/QuestionView.swift
@@ -14,13 +14,6 @@ class QuestionView: UIView {
         case correct, incorrect, standard
     }
     
-    var title = "" {
-        didSet {
-//            modification de la question ici
-            label.text = title
-        }
-    }
-    
     var style: Style = .standard {
         didSet {
             setStyle(style)
@@ -29,24 +22,28 @@ class QuestionView: UIView {
     
     private func setStyle(_ style: Style) {
         switch style {
-            case .correct:
-    //            2 versions using UIColor or Color Literal
-    //             backgroundColor = UIColor(red: 200.0/255.0, green: 236.0/255.0, blue: 160.0/255.0, alpha: 1) // Vert
-                backgroundColor = #colorLiteral(red: 0, green: 0.8385797143, blue: 0.6182498932, alpha: 1)
-                icon.image = UIImage(named: "Icon Correct")
-                icon.isHidden = false
-            case .incorrect:
-    //             backgroundColor = UIColor(red: 243.0/255.0, green: 135.0/255.0, blue: 148.0/255.0, alpha: 1) // Rouge
-                backgroundColor = #colorLiteral(red: 1, green: 0.2546873391, blue: 0.2968834937, alpha: 1)
-                icon.image = UIImage(named: "Icon Incorrect")
-                icon.isHidden = false
-            case .standard:
-    //            backgroundColor = UIColor(red: 191.0/255.0, green: 196.0/255.0, blue: 201.0/255.0, alpha: 1) // Gris
-                backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
-                icon.isHidden = true
+        case .correct:
+//            backgroundColor = UIColor(red: 200.0/255.0, green: 236.0/255.0, blue: 160.0/255.0, alpha: 1)
+            backgroundColor = #colorLiteral(red: 0, green: 0.8385797143, blue: 0.6182498932, alpha: 1)
+            icon.image = #imageLiteral(resourceName: "Icon Correct")
+            icon.isHidden = false
+        case .incorrect:
+//            backgroundColor = UIColor(red: 243.0/255.0, green: 135.0/255.0, blue: 148.0/255.0, alpha: 1)
+            backgroundColor = #colorLiteral(red: 1, green: 0.2546873391, blue: 0.2968834937, alpha: 1)
+            icon.image = #imageLiteral(resourceName: "Icon Error")
+            icon.isHidden = false
+        case .standard:
+//            backgroundColor = UIColor(red: 191.0/255.0, green: 196.0/255.0, blue: 201.0/255.0, alpha: 1)
+            backgroundColor = #colorLiteral(red: 0.8143808246, green: 0.8472908139, blue: 0.8818879128, alpha: 1)
+            icon.isHidden = true
         }
     }
     
+    var title = "" {
+        didSet {
+            label.text = title
+        }
+    }
     @IBOutlet private var label: UILabel!
     @IBOutlet private var icon: UIImageView!
 }
