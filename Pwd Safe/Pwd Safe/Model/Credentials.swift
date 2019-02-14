9c2d688a40e450ccb072fac3408e58d2985517d0
Xelaflash <xelaflash@gmail.com>
2019-02-14 10:20:55 -0400

    credentials class creation + credential manager functions

--- Pwd Safe/Pwd Safe/Model/Credentials.swift
+++ Pwd Safe/Pwd Safe/Model/Credentials.swift
@@ -14,4 +14,49 @@ class Credentials: Object {
     @objc private dynamic var _login = ""
     @objc private dynamic var _password = ""
     @objc private dynamic var _url = ""
+    
+    var title:String {
+        get {
+            return _title
+        }
+        set {
+            realm?.beginWrite()
+            _title = newValue
+            try? realm?.commitWrite()
+        }
+    }
+    
+    var login:String {
+        get {
+            return _login
+        }
+        set {
+            realm?.beginWrite()
+            _login = newValue
+            try? realm?.commitWrite()
+        }
+    }
+    
+    var password:String {
+        get {
+            return _password
+        }
+        set {
+            realm?.beginWrite()
+            _password = newValue
+            try? realm?.commitWrite()
+        }
+    }
+    
+    var url:String {
+        get {
+            return _url
+        }
+        set {
+            realm?.beginWrite()
+            _url = newValue
+            try? realm?.commitWrite()
+        }
+    }
+    
 }
